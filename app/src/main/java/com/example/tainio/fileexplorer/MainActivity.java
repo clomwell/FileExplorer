86be5c5bf950bae2ac462ce86ef3eacb5a91b3f9
tainio <clomwell@gmail.com>
2015-04-25 01:35:53 +0900

    listview onitemclick

--- app/src/main/java/com/example/tainio/fileexplorer/MainActivity.java
+++ app/src/main/java/com/example/tainio/fileexplorer/MainActivity.java
@@ -3,20 +3,27 @@ package com.example.tainio.fileexplorer;
 import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends ActionBarActivity {
 
-    private List<String> item = null;
-    private List<String> path = null;
+    private List<String> item = new ArrayList<String>();
+    private List<String> path = new ArrayList<String>();
     private String mroot;
     private TextView myPath;
+    private ListView filelv;
+    private ArrayAdapter<String> fileList;
+    private File file;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,35 +31,56 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         myPath = (TextView)findViewById(R.id.path);
+        filelv = (ListView) findViewById(R.id.lv_file);
         mroot = Environment.getExternalStorageDirectory().getParent();
-        getDir(mroot);
+        moveDir(mroot);
     }
 
-    private void getDir(String dirPath){
-        item = new ArrayList<String>();
-        path = new ArrayList<String>();
-        File file = new File(dirPath);
+    private void nowDir(String dirPath) throws IOException {
+        item.clear();//item = new ArrayList<String>();
+        path.clear();//path = new ArrayList<String>();
+        file = new File(dirPath);
         File[] mfilelist = file.listFiles();
 
+        myPath.setText(dirPath);
+//        Toast.makeText(getApplicationContext(), "1" + dirPath + "1" + mroot + "1", Toast.LENGTH_SHORT).show();
+//        if(mroot == Environment.getExternalStorageDirectory().getParent()){
+//            item.add("..");
+//        }
 
         for(int i=0; i < mfilelist.length; i++)
         {
             file = mfilelist[i];
-
             if(!file.isHidden() && file.canRead()){
                 path.add(file.getPath());
                 if(file.isDirectory()){
-                    item.add(file.getName() + "/");
+                    item.add("/" + file.getName());
                 }else{
                     item.add(file.getName());
                 }
             }
         }
 
-        ArrayAdapter<String> fileList = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, item);
-        ListView listView = (ListView) findViewById(R.id.lv_file);
-        listView. setAdapter(fileList);
-        myPath.setText(file.getAbsolutePath());
+        fileList = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, item);
+
+        filelv.setAdapter(fileList);
+        filelv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                TextView ftv = (TextView) view;
+                //Toast.makeText(getApplicationContext(), file.getAbsolutePath() + ftv.getText() , Toast.LENGTH_SHORT).show();
+                mroot = mroot + ftv.getText();
+                moveDir(mroot);
+                //Toast.makeText(getApplicationContext(), mroot , Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
+    private void moveDir(String dirPath){
+        try {
+            nowDir(dirPath);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
